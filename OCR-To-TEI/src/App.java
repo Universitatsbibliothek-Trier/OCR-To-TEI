99f4fc7152710fe7adc6821829246666f2b1a93c
René Ackels <ackels@uni-trier.de>
2023-01-30 14:05:37 +0100

    validator examples

--- OCR-To-TEI/src/App.java
+++ OCR-To-TEI/src/App.java
@@ -1,6 +1,15 @@
-public class App {
+public class App 
+{
+ 
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+        // String xsdPath="OCR-To-TEI/src/resources/testfile.xsd";
+        // String xmlPath="OCR-To-TEI/src/resources/testfile1_example.xml";
+        String xsdPath="OCR-To-TEI/src/resources/testfile.xsd";
+        String xmlPath="OCR-To-TEI/src/resources/testfile1_example.xml";
+        System.out.println("testfile xml validates against testfile.xsd" + XMLValidator.validateXMLSchema(xsdPath, xmlPath) );
+
     }
+
+   
 }
