66a55b3853a80290f18b7838647864a4e398c249
René Ackels <ackels@uni-trier.de>
2023-03-21 15:21:43 +0100

    Merge branch 'dev/16-titel-in-metadaten-soll-auf-punkt-enden'

--- OCR-To-TEI/src/test/java/de/uni_trier/bibliothek/TestHelperClass.java
+++ OCR-To-TEI/src/test/java/de/uni_trier/bibliothek/TestHelperClass.java
@@ -3,15 +3,24 @@ package de.uni_trier.bibliothek;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import javax.xml.transform.TransformerException;
+import org.custommonkey.xmlunit.XMLTestCase;
+
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
 
 import de.uni_trier.bibliothek.xml.mods.ModsUnmarshaller;
 import de.uni_trier.bibliothek.xml.mods.model.generated.ModsCollection;
@@ -21,26 +30,22 @@ import de.uni_trier.bibliothek.xml.ocr.model.generated.PcGts;
 import de.uni_trier.bibliothek.xml.tei.TEICreator;
 import de.uni_trier.bibliothek.xml.tei.TEIMarshaller;
 import de.uni_trier.bibliothek.xml.tei.model.generated.TEI;
+import jakarta.xml.bind.JAXBException;
 
-public class Main {
 
-	public static void main(String[] args) throws Exception 
+public class TestHelperClass extends XMLTestCase
+{
+
+	public static <T> String createTEIandCSV(String teiPathNameFile, String ocrFolderPathString, String modsFilePath, String parametersPath, Class<T> testClass) throws JAXBException, IOException
 	{
-		// parse command-line arguments
-		CmdLineParser cmdLineParser = new CmdLineParser(args);
-		String modsPath = cmdLineParser.getModsPath();
-		String ocrFolderName = cmdLineParser.getOCRFolderName();
-		String teiPathNameFile = cmdLineParser.getTEIPathNameFile();
-		String parametersPath = "../parameters/parameters.xml";
-	
-		// create Java object with data from XML file after unmarshalling
-		InputStream inputStream = new FileInputStream(modsPath);
-		Reader xmlReader = new InputStreamReader(inputStream);
+		// create modscollection
+		InputStream modsFile = testClass.getResourceAsStream(modsFilePath);
+		Reader xmlReader = new InputStreamReader(modsFile);
 		ModsCollection modsCollection = ModsUnmarshaller.unmarshal(xmlReader);
-		xmlReader.close();
 
-		// get files from folder, check for ".xml-files" and sort them
-		File ocrFile = new File(ocrFolderName);
+		// get xml files from folder
+		URL ocrFolderPath = testClass.getResource(ocrFolderPathString);
+		File ocrFile = new File(ocrFolderPath.getPath());
 		File[] ocrFiles = ocrFile.listFiles(new FileFilter() {
 			public boolean accept(File ocrFolderName) {
 				String fileName = ocrFolderName.getName().toLowerCase();
@@ -48,12 +53,9 @@ public class Main {
 			}
 		});
 		Arrays.sort(ocrFiles);
-
-		// create csv with header and get path of inputted TEI filename
 		String teiPathName = CSVCreator.createTEIPathName(teiPathNameFile);
-		File file = CSVCreator.createFile();
+		File csvFile = CSVCreator.createFile();
 
-		// create list of PcGts Objects from folder files
 		ArrayList<PcGts> pcgtsList = new ArrayList<PcGts>();
 		for (File fileName : ocrFiles) 
 		{	
@@ -61,19 +63,24 @@ public class Main {
 			xmlReader = new InputStreamReader(inputStreamPcgts);
 			PcGts pcgtsObject = PcGtsUnmarshaller.unmarshal(xmlReader);
 			String fileNameString = fileName.getName();
-			OcrDataReader.csvPageNameComments(fileNameString, pcgtsObject, file);
+			OcrDataReader.csvPageNameComments(fileNameString, pcgtsObject, csvFile);
 			pcgtsList.add(pcgtsObject);
 		}
 		xmlReader.close();
-
+		
 		// create TEI from modsCollection-object and list of PcGts-objects
 		TEI teiObject = TEICreator.createTEI(modsCollection, pcgtsList, parametersPath);
 		String teiXmlString = TEIMarshaller.marshall(teiObject);
-		
-		// write TEI as file
 		Path teiFilePath = Paths.get(teiPathNameFile);
 		Files.writeString(teiFilePath, teiXmlString, StandardCharsets.UTF_8);	
 		System.out.println("TEI and .csv created in: " + teiPathName);	
+		return teiXmlString;
 	}
 
+	public static String createXMLfromFile(String teiPath) throws TransformerException, FileNotFoundException
+	{
+		XML xml = new XMLDocument(new File(teiPath));
+        String xmlString = xml.toString();   
+		return xmlString;
+	}
 }
