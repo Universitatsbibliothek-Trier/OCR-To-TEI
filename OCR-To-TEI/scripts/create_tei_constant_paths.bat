e70743e0a2e8a872229c57889ed78069921f4aa7
René Ackels <ackels@uni-trier.de>
2023-04-12 16:15:19 +0200

    delete unnecessary bat

--- OCR-To-TEI/scripts/create_tei_constant_paths.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set modsPath=\home\ackels\Dokumente\ocr-to-tei-pipeline\mods1.xml
-set ocrPath=\home\ackels\Dokumente
-set teiPathName=\home\ackels\Dokumente\ocr-to-tei-pipeline-1\TEI_CSV_Output\TEI_created_Band1.xml
-mvn -q -e compile exec:java -Dexec.arguments="-m,%modsPath%,-o,%ocrPath%,-t,%teiPathName%"
