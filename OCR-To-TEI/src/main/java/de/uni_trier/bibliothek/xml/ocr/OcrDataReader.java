33baf033ebcac123a284c3e97f3bcb7b68dceddc
René Ackels <ackels@uni-trier.de>
2023-04-24 10:29:33 +0200

    format

--- OCR-To-TEI/src/main/java/de/uni_trier/bibliothek/xml/ocr/OcrDataReader.java
+++ OCR-To-TEI/src/main/java/de/uni_trier/bibliothek/xml/ocr/OcrDataReader.java
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 package de.uni_trier.bibliothek.xml.ocr;
 
 import java.io.File;
