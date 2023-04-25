5804ab2bd2188dc0ec929948953c932a4bd4ff84
René Ackels <ackels@uni-trier.de>
2023-04-25 15:51:36 +0200

    license updated

--- OCR-To-TEI/scripts/startWithArguments.sh
+++ OCR-To-TEI/scripts/startWithArguments.sh
@@ -1,5 +1,4 @@
 # Copyright (c) 2023 René Ackels
-# Permission is hereby granted, free of charge, to any person obtaining
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
